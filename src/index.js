cdc2907717f0e19d014e386b133cab843ac19d2a
natacha <nat_bartu@hotmail.com>
2024-02-07 12:02:51 +1000

    create a save function to  save the data from the increment button and display it into dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 let countEl = document.getElementById("count-el");
+let saveBtn = document.getElementById("save-btn");
+let saveEl = document.getElementById("save-el");
 
 let count = 0;
 
 function increment() {
-  count = count += 1;
+  count += 1;
   countEl.innerText = count;
+}
+
+function save() {
+  let saveNumber = +count + " - ";
+  saveEl.textContent += saveNumber;
 }
